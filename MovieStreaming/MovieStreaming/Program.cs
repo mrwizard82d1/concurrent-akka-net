5ac2e39b7403c0db434fdd1cf89c5df1fb6e9cdf
Larry Jones <larry.jones@reveal-energy.com>
2019-04-06 20:44:35 -0500

    Add MovingStreaming console application

---
+++ MovieStreaming/MovieStreaming/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Akka.Actor;
+
+namespace MovieStreaming
+{
+    class Program
+    {
+        private static ActorSystem _movieStreamingActorSystem;
+            
+        static void Main()
+        {
+            _movieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+            
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+
+            _movieStreamingActorSystem.Terminate().Wait();
+        }
+    }
+}
