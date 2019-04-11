447c566e6d002e258e1f7b4d8802dc22cc742050
Larry Jones <larry.jones@reveal-energy.com>
2019-04-11 00:31:48 -0500

    Simple hook into pre-start and post-stop
    Although example code indicated that a post-stop method could be
    "simulated" by including the function inside a call to Actor<_>.Defer, I
    could not verify the behavior. I think I have coded the function
    correctly; however, it fails to print out my post-stop message

--- MovieStreaming.Fs/MovieStreaming.Fs/Actors.fs
+++ MovieStreaming.Fs/MovieStreaming.Fs/Actors.fs
@@ -4,15 +4,19 @@ module Actors =
     open Akka.FSharp
     open Messages
     
-    let playbackActor system =
-        spawn system "playbackActor"
-        <| fun mailbox ->
-            let rec loop() = actor {
-                let! { MovieTitle=movieTitle; UserId=userId } = mailbox.Receive()
-                
-                printfn "Received message to play movie, \"%s,\" for user, %d." movieTitle userId
-                
-                return! loop()
-            }
-            loop()
+    let playbackActor (mailbox:Actor<_>) =
+        // This section, before the recursive loop, acts like "PreStart"
+        printfn "pre-start"
+        
+        // Register a function to be executed when the mailbox is destroyed. This acts like "PostStart"
+        mailbox.Defer (fun () -> printfn "post-stop")
+        
+        let rec loop() = actor {
+            let! { MovieTitle=movieTitle; UserId=userId } = mailbox.Receive()
+            
+            printfn "Received message to play movie, \"%s,\" for user, %d." movieTitle userId
+            
+            return! loop()
+        }
+        loop()
 
--- MovieStreaming.Fs/MovieStreaming.Fs/Program.fs
+++ MovieStreaming.Fs/MovieStreaming.Fs/Program.fs
@@ -1,20 +1,34 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Threading
 open Akka.FSharp
 open MovieStreaming.Fs.Messages
 open MovieStreaming.Fs.Actors
+
+let prompt promptFunc =
+    promptFunc()
+    Console.ReadKey()
+    
+let defaultPrompt () =
+    prompt (fun () -> printfn "Press any key to continue...")
+    |> ignore
 
 [<EntryPoint>]
 let main argv =
     let actorSystem = System.create "MovieStreamingActorSystem" (Configuration.defaultConfig())
     printfn "Actor system, 'MovieStreamingActorSystem,' created."
         
-    let actor = playbackActor actorSystem
-    actor <! { MovieTitle="Akka.NET: The Movie"; UserId=42  }
+    let actorRef = spawn actorSystem "PlaybackActor" (playbackActor)
     
-    printfn "Press ENTER to continue..."
-    Console.ReadLine() |> ignore
+    actorRef <! { MovieTitle="Akka.NET: The Movie"; UserId=42  }
+    actorRef <! { MovieTitle="Partial Recall"; UserId=99  }
+    actorRef <! { MovieTitle="Boolean Lies"; UserId=77  }
+    actorRef <! { MovieTitle="Codenan the Destroyer"; UserId=1  }
+    
+    defaultPrompt()
+    
+    Thread.Sleep(500)
     
     actorSystem.Terminate() |> Async.AwaitTask |> ignore
     
