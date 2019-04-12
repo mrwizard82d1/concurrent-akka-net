126b089e9b421ed4b8f737043cc7fc4209eeb014
Larry Jones <larry.jones@reveal-energy.com>
2019-04-11 20:30:11 -0500

    Create a PlaybackActor

---
+++ MovieStreaming.FsClass/MovieStreaming.FsClass/Program.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Akka.Actor
+
+module Actors =
+    type PlaybackActor() =
+        inherit UntypedActor()
+        
+        do printfn "Creating a PlaybackActor."
+        
+        override this.OnReceive(message:obj) =
+            raise <| NotImplementedException()
+
+[<EntryPoint>]
+let main argv =
+    let movieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem")
+    printfn "Actor system created."
+    
+    let playbackActorProps = Props.Create<Actors.PlaybackActor>()
+    let playbackActorRef = movieStreamingActorSystem.ActorOf(playbackActorProps, "PlaybackActor")
+    
+    printfn "Press ENTER to continue..."
+    Console.ReadLine() |> ignore
+    
+    movieStreamingActorSystem.Terminate() |> Async.AwaitTask |> ignore
+    
+    0 // return an integer exit code
