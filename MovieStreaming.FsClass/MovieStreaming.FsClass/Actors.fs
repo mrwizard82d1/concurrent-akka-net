9b0b89a4f51df252f210628b0b7a2ff46023fc5c
Larry Jones <larry.jones@reveal-energy.com>
2019-04-11 21:26:45 -0500

    Change implementation to ReceiveActor

--- MovieStreaming.FsClass/MovieStreaming.FsClass/Actors.fs
+++ MovieStreaming.FsClass/MovieStreaming.FsClass/Actors.fs
@@ -5,17 +5,16 @@ module Actors =
     open Messages
 
     type PlaybackActor() =
-        inherit UntypedActor()
+        inherit ReceiveActor()
         
         do printfn "Creating a PlaybackActor."
         
-        override this.OnReceive(message:obj) =
-            match message with
-            | :? PlayMovieMessage as playMovieMessage ->
-                let { MovieTitle=movieTitle; UserId=userId } = playMovieMessage
-                printfn "Received movie title, \"%s.\"" movieTitle
-                printfn "Received user id: %d." userId
-            | _ -> this.Unhandled(message)
+        let handlePlayMovieMessage({ MovieTitle=movieTitle; UserId=userId }) =
+            printfn "Received movie title, \"%s.\"" movieTitle
+            printfn "Received user id: %d." userId
+            
+        do base.Receive<PlayMovieMessage>(fun m -> handlePlayMovieMessage(m))
+        
             
 
 
